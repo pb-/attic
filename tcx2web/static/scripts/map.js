b50572c0c31c2d68a8ab6ab5cf3d754d68a4a66f
Paul Baecher <pbaecher@gmail.com>
2010-02-06 22:18:07 +0100

    start working on profile position visualization

--- tcx2web/static/scripts/map.js
+++ tcx2web/static/scripts/map.js
@@ -1,47 +1,51 @@
+var map;
+var lgpx;
 
-/*		var lat=50.2309
-		var lon=8.879
-		var zoom=13*/
+function dataReady() {
+	map.zoomToExtent(lgpx.getDataExtent());
+}
 
-		var map;
-		var lgpx;
+function initMap() {
 
-		function dataReady() {
-			map.zoomToExtent(lgpx.getDataExtent());
-		}
+	map = new OpenLayers.Map ("map", {
+		controls:[
+			new OpenLayers.Control.Navigation(),
+			new OpenLayers.Control.PanZoomBar(),
+			new OpenLayers.Control.LayerSwitcher()
+			],
+		maxExtent: new OpenLayers.Bounds(-20037508.34,-20037508.34,20037508.34,20037508.34),
+		maxResolution: 156543.0399,
+		numZoomLevels: 19,
+		units: 'm',
+		projection: new OpenLayers.Projection("EPSG:900913"),
+		displayProjection: new OpenLayers.Projection("EPSG:4326")
+	});
 
-		function init() {
+	layerMapnik = new OpenLayers.Layer.OSM.Mapnik("Default");
+	map.addLayer(layerMapnik);
+	layerCycleMap = new OpenLayers.Layer.OSM.CycleMap("Cycle Map");
+	map.addLayer(layerCycleMap);
 
-			map = new OpenLayers.Map ("map", {
-				controls:[
-					new OpenLayers.Control.Navigation(),
-					new OpenLayers.Control.PanZoomBar(),
-					new OpenLayers.Control.LayerSwitcher()
-					],
-				maxExtent: new OpenLayers.Bounds(-20037508.34,-20037508.34,20037508.34,20037508.34),
-				maxResolution: 156543.0399,
-				numZoomLevels: 19,
-				units: 'm',
-				projection: new OpenLayers.Projection("EPSG:900913"),
-				displayProjection: new OpenLayers.Projection("EPSG:4326")
-			} );
-			layerMapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
-			map.addLayer(layerMapnik);
-			layerCycleMap = new OpenLayers.Layer.OSM.CycleMap("CycleMap");
-			map.addLayer(layerCycleMap);
+	lgpx = new OpenLayers.Layer.GML("Track", "light.gpx", {
+		format: OpenLayers.Format.GPX,
+		style: {strokeColor: "#9600ff", strokeWidth: 5, strokeOpacity: 0.5},
+		projection: new OpenLayers.Projection("EPSG:4326"),
+		eventListeners: { 'loadend': dataReady }
+	});
 
-                        lgpx = new OpenLayers.Layer.GML("Track", "light.gpx", {
-                            format: OpenLayers.Format.GPX,
-                            style: {strokeColor: "#9600ff", strokeWidth: 5, strokeOpacity: 0.5},
-                            projection: new OpenLayers.Projection("EPSG:4326"),
-							eventListeners: { 'loadend': dataReady }
-                        });
-                        map.addLayer(lgpx);
+	map.addLayer(lgpx);
 
-			lgpx.loadGML()
-//			map.zoomToExtent(lgpx.getDataExtent());
- 
-		//	var lonLat = new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
-		//	map.setCenter (lonLat, zoom);
-		}
+	lgpx.loadGML();
 
+	$('graphAltitude').addEvent('mousemove', graphMouseMove);
+	$('graphSpeed').addEvent('mousemove', graphMouseMove);
+	$('graphCadence').addEvent('mousemove', graphMouseMove);
+}
+
+function graphMouseMove(event) {
+	alert('move!');
+}
+
+window.addEvent('domready', function() {
+	initMap();
+});
